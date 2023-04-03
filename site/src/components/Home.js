4a1dfe6d3f65b0b77c56c2d67d0b836e59004668
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-04-03 19:05:54 +0200

    setup gh-pages

---
+++ site/src/components/Home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+
+  return (
+    <div>
+      <h1>Welcome !</h1>
+      <p>My name is Mickael Louvel, I'm 24 years old, I live in France and I'm currently a student
+        at University of Le Havre. I love programming, watching football (soccer for american people) and praticing
+        weightlifting.</p>
+    </div>
+  )
+}
