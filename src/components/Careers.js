2983dc276c141e8d7e09c3d69177874338c2f5b5
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2023-04-05 16:43:25 +0200

    continue CSS

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -10,13 +10,17 @@ export default function Careers() {
   return (
     <div>
       <title>Careers</title>
-      <div className="flex flex-col items-center ml-5 mt-5 border border-4 border-teal-500 rounded-lg w-1/2 h-fit">
+      <div
+        className={`flex flex-col items-start ml-5 mt-3 border border-4 border-teal-500 rounded-lg w-fit h-fit ease-in-out duration-500`}
+      >
         <h1 className="w-full text-left text-white bg-teal-500 text-2xl p-2">
           School career
         </h1>
-        <div className={"grid grid-cols-2 gap-2 mt-3 text-l p-2"}>
+        <div
+          className={`flex flex-col p-2 ease-in-out duration-500 overflow-y-auto`}
+        >
           <div
-            className={`ml-5 w-max max-w-screen-sm cursor-pointer ease-in-out duration-300 ${
+            className={`ml-2 w-max max-w-screen-sm cursor-pointer ease-in-out duration-500 ${
               expandSections.section_1
                 ? "border border-2 border-teal-500 rounded-lg"
                 : ""
@@ -26,7 +30,7 @@ export default function Careers() {
             }
           >
             <div
-              className={`ease-in-out duration-300 ${
+              className={`ease-in duration-500 ${
                 expandSections.section_1
                   ? "bg-teal-500 text-white rounded-sm p-2 w-full"
                   : ""
@@ -34,71 +38,110 @@ export default function Careers() {
             >
               Master degree in Computer Science at{" "}
               <a
-                className={`${
+                className={`ease-in duration-500 ${
                   expandSections.section_1
                     ? "bg-teal-500 text-white hover:underline"
                     : "text-teal-600 hover:underline"
                 }`}
                 href="https://www.univ-lehavre.fr/"
               >
-                University of Le Havre
+                University of Le Havre (2021-2023)
               </a>
             </div>
             {expandSections.section_1 ? (
-              <div className="p-2 overflow-y-auto w-fit">
-                <div className="border border-2 border-teal-500 rounded-lg p-2">
-                  <h2 className="bg-teal-500 rounded-lg p-2 text-white w-fit">
-                    First year
-                  </h2>
-                  <ul className="break-words">
-                    <li className="border-l-2 border-black px-2 mt-2">
-                      Learned{" "}
-                      <a
-                        className="text-teal-500 hover:underline"
-                        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-                      >
-                        JavaScript
-                      </a>{" "}
-                      and its frameworks{" "}
-                      <a
-                        className="text-teal-500 hover:underline"
-                        href="https://nodejs.org/en"
-                      >
-                        Node
-                      </a>{" "}
-                      and{" "}
-                      <a
-                        className="text-teal-500 hover:underline"
-                        href="https://react.dev/ "
-                      >
-                        ReactJS
-                      </a>
-                    </li>
-                    <li className="border-l-2 border-black px-2 mt-2">
-                      Learned{" "}
-                      <a
-                        className="text-teal-500 hover:underline"
-                        href="https://www.scala-lang.org/"
-                      >
-                        Scala
-                      </a>{" "}
-                      and discovered new programming paradigm :{" "}
-                      <a
-                        className="text-teal-500 hover:underline"
-                        href="https://en.wikipedia.org/wiki/Functional_programming"
-                      >
-                        Functional Programming
-                      </a>
-                    </li>
-                  </ul>
-                </div>
+              <div className="ease-in-out duration-500 p-2 w-fit">
+                <h2 className="bg-teal-500 rounded-lg p-2 text-white w-fit">
+                  First year
+                </h2>
+                <ul className="break-words">
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Learned{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+                    >
+                      JavaScript
+                    </a>{" "}
+                    and its frameworks{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://nodejs.org/en"
+                    >
+                      Node
+                    </a>{" "}
+                    and{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://react.dev/ "
+                    >
+                      ReactJS
+                    </a>
+                  </li>
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Learned{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://www.scala-lang.org/"
+                    >
+                      Scala
+                    </a>{" "}
+                    and discovered new programming paradigm :{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://en.wikipedia.org/wiki/Functional_programming"
+                    >
+                      Functional Programming
+                    </a>
+                  </li>
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Discovered new implementation of database by using{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://en.wikipedia.org/wiki/Document_type_definition"
+                    >
+                      DTD
+                    </a>{" "}
+                    and XML.
+                  </li>
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Developped a CLI program in Python for manipulating and
+                    extracting data from CSV in the context of a big project.
+                    The objective was to treat ships travel records provided by{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://en.wikipedia.org/wiki/Automatic_identification_system"
+                    >
+                      AIS
+                    </a>{" "}
+                    and allow user to extract any information he wants from one
+                    or many CSV files. User cought type args defined by the
+                    program and get output file as return.
+                  </li>
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Developped in{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://processing.org/"
+                    >
+                      Processing
+                    </a>{" "}
+                    in the context of learning data visualisation.
+                  </li>
+                  <li className="border-l-2 border-black px-2 mt-2">
+                    Introduced to{" "}
+                    <a
+                      className="text-teal-500 hover:underline"
+                      href="https://en.wikipedia.org/wiki/Artificial_intelligence"
+                    >
+                      AI
+                    </a>
+                    .
+                  </li>
+                </ul>
               </div>
             ) : null}
           </div>
-          <div className="border-l-2 border-black px-5 col-end-5">
-            2021-2023
-          </div>
-          <div className="ml-5 w-max">
+          <div className="ml-2 w-max">
             Bachelor degree in Computer Science at{" "}
             <a
               className="text-teal-600 hover:underline"
@@ -107,13 +150,9 @@ export default function Careers() {
               University of Le Havre
             </a>
           </div>
-          <div className="border-l-2 border-black px-5 col-end-5">
-            2017-2021
-          </div>
-          <div className="ml-5 w-max">
+          <div className="ml-2 w-max">
             High School diploma in Sciences, speciality Biology
           </div>
-          <div className="border-l-2 border-black px-5 col-end-5">2017</div>
         </div>
       </div>
     </div>
