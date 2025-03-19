cfa1082f01f85b4a27b65f010841e68b04437b3b
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2025-03-19 19:02:09 +0100

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/Portfolio",
+  assetPrefix: isProd ? "/Portfolio" : "",
+  basePath: isProd ? "/Portfolio" : "",
   output: "export",
   reactStrictMode: true,
 };
