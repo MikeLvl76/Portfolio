a83ab97d81166d967e0804ac2e07d5d913ddb2e3
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-04-13 16:53:37 +0200

    make navbar responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -8,8 +8,8 @@ type Props = {
 
 export default function Navbar({ children }: Props) {
   return (
-    <div className="flex flex-row justify-start items-center p-2 gap-8 w-full h-[10%] shadow-2xl/30 fixed z-1 bg-slate-100">
+    <nav className="flex flex-col items-center justify-start sm:flex-row sm:justify-start sm:items-center px-3 sm:px-4 md:px-6 lg:px-8 gap-3 sm:gap-4 md:gap-6 lg:gap-8 w-full sm:min-h-1/12 shadow-2xl/30 top-0 left-0 fixed z-50 bg-slate-100">
       {children}
-    </div>
+    </nav>
   );
 }
