1f3b9dc111935695b61445429004a1207d30cde4
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-04-15 14:43:14 +0200

    fix github pages deployment and add basepath on svg filepath

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
+const repo = process.env.REPO_NAME;
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  assetPrefix: isProd ? "/Portfolio" : "",
-  basePath: isProd ? "/Portfolio" : "",
+  basePath: isProd && repo ? `/${repo}` : "",
+  assetPrefix: isProd && repo ? `/${repo}/` : "",
   output: "export",
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
