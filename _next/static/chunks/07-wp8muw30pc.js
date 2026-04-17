47903149b48624fc0cbeeba13f7895e47222a5a1
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-04-17 23:20:37 +0200

    Updates

--- _next/static/chunks/07-wp8muw30pc.js
+++
@@ -1,1 +0,0 @@
-(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,62727,e=>{"use strict";var t=e.i(43476);e.s(["default",0,function({children:e}){return(0,t.jsx)("div",{className:"flex justify-center items-center max-w-screen min-h-screen",children:e})}])}]);
