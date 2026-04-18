f87fa8f68fab3ea67b8dd0b6ee75db95b18cfcfc
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2026-04-18 21:51:09 +0200

    add languages json to translate depending on user navigator language

--- src/components/navbar/navbar-item.tsx
+++ src/components/navbar/navbar-item.tsx
@@ -7,16 +7,21 @@ type Props = {
   to: string;
   icon?: LucideIcon;
   label?: string;
+  title?: string;
 };
 
-export default function NavbarItem({ to, icon: Icon, label }: Props) {
+export default function NavbarItem({ to, icon: Icon, label, title }: Props) {
   return (
     <Link
       href={to}
+      title={title}
       className="flex flex-row items-center justify-center gap-2 hover:decoration-0 sm:p-2 w-fit h-fit md:h-full hover:bg-[#dddddd] rounded-sm"
     >
       {Icon && (
-        <Icon fill="#eeeeee" className="hover:cursor-pointer size-4 sm:size-6" />
+        <Icon
+          fill="#eeeeee"
+          className="hover:cursor-pointer size-4 sm:size-6"
+        />
       )}
       {label && (
         <label className="text-[11px] sm:text-base lg:text-lg font-bold hover:cursor-pointer">
